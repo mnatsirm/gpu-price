93def3b7771f441a0a7bb1e9594205930d57c648
mnatsirm <mnatsirm53@gmail.com>
2021-06-01 12:53:09 +0700

    stuck to trigger function every specific time

--- src/gpu-price-data.js
+++ src/gpu-price-data.js
@@ -6,14 +6,14 @@ export const gpuPriceChartData = {
 		labels: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
 		datasets: [
 			{
-				label: "Number of Moons",
+				label: "msi",
 				data: [0, 0, 1, 2, 79, 82, 27, 14],
 				backgroundColor: "rgba(54,73,93,.5)",
 				borderColor: "#36495d",
 				borderWidth: 3
 			},
 			{
-				label: "Planetary Mass (relative to the Sun x 10^-6)",
+				label: "gigabyte",
 				data: [0.166, 2.081, 3.003, 0.323, 954.792, 285.886, 43.662, 51.514],
 				backgroundColor: "rgba(71, 183,132,.5)",
 				borderColor: "#47b784",
@@ -37,14 +37,20 @@ export const gpuPriceChartData = {
 	}
 };
 
+/*
+	TODO:
+		1/ create function to get current date
+		2/ create function to get price
+		3/ create function to push to data every specific time
+*/
+
+
 function pushDate() {
 	return moment().format('YYYY-MM-DD');
 }
 
-
 gpuPriceChartData.data.labels.push(pushDate())
 gpuPriceChartData.data.datasets[0].data.push("420")
 gpuPriceChartData.data.datasets[1].data.push("420")
-
 
 export default gpuPriceChartData;
