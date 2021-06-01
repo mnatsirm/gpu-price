66932f4b764fec3971b759751a0958c324a2633a
mnatsirm <mnatsirm53@gmail.com>
2021-06-01 11:19:49 +0700

    boilerplate project

---
+++ src/components/GPUPriceChart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <canvas id="gpu-price-chart"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js'
+import gpuPriceChartData from '../gpu-price-data.js'
+
+export default {
+  name: 'GPUPriceChart',
+  data() {
+    return {
+      gpuPriceChartData: gpuPriceChartData
+    }
+  },
+  mounted() {
+    const ctx = document.getElementById('gpu-price-chart');
+    new Chart(ctx, this.gpuPriceChartData);
+  }
+}
+</script>
